5e877bf98002e70009e5df95fdb8fe3ac8e04d63
Shubham Matale <mataleshubham@gmail.com>
2021-05-09 17:38:24 +0530

    Added code for the promocode table

---
+++ database/migrations/2021_05_09_173327_create_promocodes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromocodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promocodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('promocode');
+            $table->string('type');
+            $table->decimal('discount');
+            $table->decimal('minimal_cart_total');
+            $table->decimal('max_discount');
+            $table->boolean('is_for_new_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promocodes');
+    }
+}
